ba4eadaa0d2c26317c3e9f21b629c5f531eff598
Jerry Mendoza <jamendoza@uc.cl>
2020-10-25 02:40:13 -0300

    Added custom token response

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require "custom_token_response"
 Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (requires ORM extensions installed).
   # Check the list of supported ORMs here: https://github.com/doorkeeper-gem/doorkeeper#orms
@@ -503,3 +503,4 @@ Doorkeeper.configure do
   #
   # realm "Doorkeeper"
 end
+Doorkeeper::OAuth::TokenResponse.send :prepend, CustomTokenResponse
