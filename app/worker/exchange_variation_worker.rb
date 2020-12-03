78babc7f8cfbd4432f3c326e05693dcb0163f68b
nfbalbontin <nfbalbontin@uc.cl>
2020-12-03 20:36:23 -0300

    fixed problemas

--- app/worker/exchange_variation_worker.rb
+++ app/worker/exchange_variation_worker.rb
@@ -3,7 +3,7 @@ class ExchangeVariationWorker
   
   def perform
     loop do
-      sleep 10
+      sleep 10800
       random_value = rand(-0.2..0.2)
       Coin.all.each do |coin|
         coin.precio_venta = coin.precio_venta + coin.precio_venta * random_value
