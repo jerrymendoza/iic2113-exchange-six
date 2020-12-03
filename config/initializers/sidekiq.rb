c6eaf1faf7b66ace08289877ca198f21e07cfcba
nfbalbontin <nfbalbontin@uc.cl>
2020-12-02 22:58:01 -0300

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
-# Sidekiq.configure_server do |_config|
-#   ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
-# end
+Sidekiq.configure_server do |_config|
+  ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
+end
 
-#if Rails.env.production?
+if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
+    config.redis = { url: ENV["REDISTOGO_URL"] }
   end
-#end
+end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"] }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
 end
